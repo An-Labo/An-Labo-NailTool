c727360b0eb606d6f1c51e9dd5aa8e0c40c3e74c
narugami <kazunari_kida@skeb.co.jp>
2024-07-25 14:27:02 +0900

    Avatar Optimizerに対応。
    現在アバターの最適化ツールとしてデファクトスタンダードになりつつある[Avatar Optimizer](https://github.com/anatawa12/AvatarOptimizer)使用時に、互換性エラーが発生する問題を修正します。
    [Avatar Optimizer公式のトラブルシューティング](https://vpm.anatawa12.com/avatar-optimizer/ja/docs/developers/make-your-components-compatible-with-aao/)に従い、ランタイムではMDNailObjectMarkerを削除するコールバックを追加しました。
    MDNailObjectMarkerはネイルの削除や変更時のみ使用するため、ランタイムではこのコンポーネントは不要だと考えます。
    ランタイムの時だけ削除されるため、改変中やAvatar Optimizerを使用していない環境での影響はありません。

---
+++ Packages/world.anlabo.mdnailtool/Editor/AAOProcessor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using VRC.SDKBase.Editor.BuildPipeline;
+using world.anlabo.mdnailtool.Runtime;
+
+namespace world.anlabo.mdnailtool.Editor {
+	public class AAOProcessor : IVRCSDKPreprocessAvatarCallback {
+		public int callbackOrder => -1025 - 1;
+
+		public bool OnPreprocessAvatar(GameObject avatarGameObject) {
+			var components = avatarGameObject.GetComponentsInChildren<MDNailObjectMarker>();
+            foreach (var component in components) {
+				Object.DestroyImmediate(component);
+			}
+			return true;
+		}
+	}
+}
