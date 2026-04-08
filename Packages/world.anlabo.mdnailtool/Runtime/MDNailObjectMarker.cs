8d282ccb1ae2c341869de2d7548dd909158f0274
ANGEL\angelic <chamomillavv@gmail.com>
2026-04-09 02:03:49 +0900

    Release 0.9.340
    ・NDMF異常時のネイル消失防止

--- Packages/world.anlabo.mdnailtool/Runtime/MDNailObjectMarker.cs
+++ Packages/world.anlabo.mdnailtool/Runtime/MDNailObjectMarker.cs
@@ -1,9 +1,8 @@
 ﻿using UnityEngine;
-using VRC.SDKBase;
 
 namespace world.anlabo.mdnailtool.Runtime {
 	[AddComponentMenu("")]
-	public class MDNailObjectMarker : MonoBehaviour, IEditorOnly {
-		
+	public class MDNailObjectMarker : MonoBehaviour {
+
 	}
 }
